92d8cce64b850da170712b735e8d5a3f7c5183b9
Dune Zerna <duzer17@student.sdu.dk>
2019-11-26 13:14:11 +0100

    Base commit
    Login screen (no validate) & Navigation controller

---
+++ MenuViewController.swift
@@ -0,0 +1,48 @@
+//
+//  MenuViewController.swift
+//  progress
+//
+//  Created by Pernille Madsen on 20/11/2019.
+//  Copyright © 2019 Pernille Madsen. All rights reserved.
+//
+
+import UIKit
+import HealthKit
+
+class MenuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupMenuView()
+    }
+    
+    fileprivate func setupMenuView() {
+        let menuViewController = storyBoard.instantiateViewController(withIdentifier: "Menu")
+        self.present(menuViewController, animated:true, completion:nil)
+        
+        
+        
+    }
+    
+    let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+    
+    
+    
+    
+}
+
+    
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Challenger Storyboard
+//
+//  Created by Dune Zerna on 22/11/2019.
+//  Copyright © 2019 University of Southern Denmark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
